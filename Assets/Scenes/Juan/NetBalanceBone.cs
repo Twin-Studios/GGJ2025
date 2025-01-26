6b2bd261228129a6a562ffa1599e1eb11464617f
Juan Polanco López <juaninpolancolopez@gmail.com>
2025-01-26 12:28:04 +0100

    update

--- Assets/Scenes/Juan/NetBalanceBone.cs
+++ Assets/Scenes/Juan/NetBalanceBone.cs
@@ -5,15 +5,22 @@ public class NetBalanceBone : MonoBehaviour
     [SerializeField] private float repositionForce = 5;
 
     private Vector3 startPos;
+    private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, startPos,Time.deltaTime * repositionForce);
+        //transform.position = Vector3.Lerp(transform.position, startPos,Time.deltaTime * repositionForce);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce((startPos - transform.position) * repositionForce);
     }
 }
